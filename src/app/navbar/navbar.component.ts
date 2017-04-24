bac45118c9cfa5fd5fb92baeabdc002ae8573383
Stephen Wright <sjw948@gmail.com>
2017-04-24 20:58:28 +0100

    finished responsive navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  menuVisible: boolean = false
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleVisibility(resize?) {
+    if(resize && screen.width < 768) {
+      this.menuVisible = false;
+      return {"visibility": "hidden", "opacity": "0"}
+    } else if (screen.width >= 768) {
+      this.menuVisible = true;
+      return {"visibility": "visible", "opacity": "1"}
+    } else if (this.menuVisible == true) {
+      return {"visibility": "visible", "opacity": "1"}
+    } else {
+      return {"visibility": "hidden", "opacity": "0"}
+    }
   }
 
 }
