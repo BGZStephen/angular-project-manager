c133e5ab9fc388027ab82e3bce90414c2a127f0c
Stephen Wright <sjw948@gmail.com>
2017-04-25 12:19:50 +0100

    built & styled project to do lists and projects component

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,7 +9,17 @@ export class ProjectsComponent implements OnInit {
 
   constructor() { }
 
+  projectVisible: boolean = false;
+
   ngOnInit() {
+  }
+
+  toggleProjectVisibility() {
+    if (this.projectVisible == false) {
+      return {"max-height": "0", "height": "0"}
+    } else {
+      return {"max-height": "9000px", "height": "auto"}
+    }
   }
 
 }
